3f36ac3929de1353a98d0c580452f2b1fd7d898d
rivmos <rivmos.official@gmail.com>
2022-03-25 15:57:33 +0530

    initial index page prototype for gosafecargo

---
+++ assets/js/main.js
@@ -0,0 +1,95 @@
+let services = {
+  corporate : 'We are the fastest growing corporate shifter in India and Big brands trust us for their shifting needs.',
+  household : 'Shifting to new house?<br> Well we are here to help you to shift with extra care.',
+  packing : 'You name it.<br>We pack and ship it.<br>Right at your doorstep.',
+  car : 'Want your cars to be transported somewhere else?<br>Guess what, We do that to.',
+  transport : 'We have partenered with the best transportation services in India and we operate pan India.',
+  warehouse : 'We are here for all your warehousing needs as well.',
+}
+let reviews = [
+  { name : 'Aditya Bansal',
+    reply : "I don't usually write reviews but gosafe is definitely helped me in every possible way"
+  },
+  { name : 'Surbhi Mankad',
+    reply : 'Thanks gosafe'
+  },
+  { name : 'Sarvalpalli',
+    reply : 'Abhishek was very helpful with the shifting'
+  },
+  { name : 'Maanik Aggarwal',
+    reply : 'Totally a lifesaver'
+  },
+  { name : 'Aaina Kapoor',
+    reply : 'Thank you for giving extra care to my collectibles'
+  },
+]
+
+
+let i=0; // For review automatic movement
+showRev();
+
+let serList = document.getElementsByClassName('indiService');
+let serTab = document.getElementById('serTab');
+let serTabHead = document.getElementById('serTabHead');
+let serTabContent = document.getElementById('serTabContent');
+
+let custName = document.getElementById('custName');
+let custReply = document.getElementById('custReply');
+
+Array.from(serList).forEach(function(element){
+    element.addEventListener('mouseover',showService);
+    element.addEventListener('blur',resetService);
+})
+
+function showService(e){
+  console.log(e.target.id)
+  currElem = document.getElementById(e.target.id);
+  currElemText = currElem.innerText;
+  serTabHead.innerText = currElemText;
+  serTabContent.innerHTML = services[e.target.id];
+}
+
+function resetService(){
+  serTabHead.innerText = 'Top Notch Services';
+}
+
+function showRev(){
+  setInterval(function(){
+    if(i<5){
+      custName.innerText = reviews[i].name;
+      custReply.innerText = reviews[i].reply;
+      i++;
+    }
+    else{
+      i = 0;
+      custName.innerText = 'Krishna';
+      custReply.innerText = "The process was totally hasslefree. I'm glad that I chose gosafe for this.";
+    }
+  },3000);
+}
+
+let mobNumber = document.getElementById('mobNumber');
+mobNumber.addEventListener('input',validateMobNumber);
+function validateMobNumber(){
+  if(Number(mobNumber.value) === NaN){
+    console.log('no. nhi hai')
+  }
+}
+
+let submitBtn = document.getElementById('submitBtn');
+submitBtn.addEventListener('click',validate)
+function validate(){
+  if(mobNumber.val != Number){
+    console.log('not a numb')
+  }
+}
+
+
+function showNav(){
+  let navigation = document.getElementById('navigation');
+  if (navigation.style.display === "block") {
+    navigation.style.display = "none";
+  } else {
+    navigation.style.display = "block";
+  }
+}
