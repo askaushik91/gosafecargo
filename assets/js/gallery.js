10e3b58fff050d6613404182d332bd0d72f667b1
rivmos <rivmos.official@gmail.com>
2022-03-26 23:20:09 +0530

    ak helped improve css code

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,16 +1,17 @@
 showImages();
+
 let images = ['two.jpeg','three.jpeg','four.jpeg'];
 i = 0;
 function showImages(){
     let image = document.getElementById('image');
     setInterval(function(){
       if(i<3){
-        image.src = `/assets/media/images/${images[i]}`;
+        image.src = `/gosafe/assets/media/images/${images[i]}`;
         i++;
       }
       else{
         i = 0;
-        image.src = '/assets/media/images/one.jpeg';
+        image.src = '/gosafe/assets/media/images/one.jpeg';
       }
     },3000);
   }
