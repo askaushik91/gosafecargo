77fdcc44c2801b980fac60cb50a79612b3f5b7a1
rivmos <rivmos.official@gmail.com>
2022-04-07 17:47:30 +0530

    fixed the navbar js code

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,10 +1,9 @@
 function showNav(){
     let navigation = document.getElementById('navigation');
     if (navigation.style.display === "block") {
-      // navigation.style.display = '@keyframes nav {100% {display:none;}}';
-      // navigation.style.display = "none";
+      navigation.style.display = 'none'
+
     } else {
       navigation.style.display = "block";
-      navigation.style.display = 'animation : fade-in 2s';
     }
   }
