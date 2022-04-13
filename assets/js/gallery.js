2af722936f7e37a247902252429a30eec44c88f6
rivmos <rivmos.official@gmail.com>
2022-04-13 13:51:45 +0530

    images slideshow added

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,19 +1,41 @@
-showImages();
+// showImages();
 
-let images = ['two.jpeg','three.jpeg','four.jpeg'];
-i = 0;
+// let images = ['two.jpeg','three.jpeg','four.jpeg'];
+// i = 0;
+// showImages();
+// function showImages(){
+//     let image = document.getElementById('galleryImage');
+//     setInterval(function(){
+//       if(i<3){
+//         image.src = `assets/media/images/${images[i]}`;
+//         i++;
+//       }
+//       else{
+//         i = 0;
+//         image.src = 'assets/media/images/one.jpeg';
+//       }
+//     },5000);
+//   }
+
+let imagesArray = ['two.jpeg','three.jpeg','four.jpeg','one.jpeg'],
+    imageElem = document.getElementById('galleryImage'),
+    imgCounter = 0,
+    imgDuration = 3000;
+
 function showImages(){
-    let image = document.getElementById('image');
+    
     setInterval(function(){
-      if(i<3){
-        image.src = `assets/media/images/${images[i]}`;
-        i++;
+      imageElem.className = 'fadeOut';
+      setTimeout(function(){
+        imageElem.src = `assets/media/images/${imagesArray[imgCounter]}`;
+        imageElem.className = '';
+      },1000);
+      imgCounter++;
+      if(imgCounter==imagesArray.length){
+        imgCounter=0;
       }
-      else{
-        i = 0;
-        image.src = 'assets/media/images/one.jpeg';
-      }
-    },3000);
+    },imgDuration);
+
   }
 
-    
+window.onload = showImages();
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,22 @@
+//services object used to for replcament in the serTab (comprosises of serTabHead and serTabContent)
 let services = {
   corporate : 'We are the fastest growing corporate shifter in India and Big brands trust us for their shifting needs.',
-  household : 'Shifting to new house?<br> Well we are here to help you to shift with extra care.',
-  packing : 'You name it.<br>We pack and ship it.<br>Right at your doorstep.',
   car : 'Want your cars to be transported somewhere else?<br>Guess what, We do that too.',
   transport : 'We have partenered with the best transportation services in India and we operate pan India.',
   warehouse : 'We are here for all your warehousing needs as well.',
+  household : 'Shifting to new house?<br> Well we are here to help you to shift with extra care.',
+  packing : 'You name it.<br>We pack and ship it.<br>Right at your doorstep.',
 }
+
+// serNameArray is used for 
+let serNameArray = ['corporate','car','transport','warehouse','household','packing'];
+
 let reviews = [
   { name : 'Aditya Bansal',
-    reply : "I don't usually write reviews but gosafe is definitely helped me in every possible way"
+    reply : "I don't usually write reviews but yesgo is definitely helped me in every possible way"
   },
   { name : 'Surbhi Mankad',
-    reply : 'Thanks gosafe'
+    reply : 'Thanks yesgo'
   },
   { name : 'Sarvalpalli',
     reply : 'Abhishek was very helpful with the shifting'
@@ -26,10 +31,10 @@ let reviews = [
 
 let i=0; // For review automatic movement
 showRev();
+// serviceAuto();
 
 
 let serList = document.getElementsByClassName('indiService');
-let serTab = document.getElementById('serTab');
 let serTabHead = document.getElementById('serTabHead');
 let serTabContent = document.getElementById('serTabContent');
 
@@ -42,11 +47,54 @@ Array.from(serList).forEach(function(element){
 })
 
 function showService(e){
-  currElem = document.getElementById(e.target.id);
-  currElemText = currElem.innerText;
+  let currElem = document.getElementById(e.target.id);
+  let currElemText = currElem.innerText;
   serTabHead.innerText = currElemText;
   serTabContent.innerHTML = services[e.target.id];
 }
+
+
+
+// function serviceAuto(){
+//   let count = 0;
+//   let serList = document.getElementById('serList');
+//   setInterval(function(){
+//       Array.from(serList.children).forEach(function(element){
+//           element.className = '';
+//       })
+//       if(count==0){
+//         serList.children[count].className = 'serActive';
+//       }
+//       else{
+//         serList.children[count-1].className = '';
+//         serList.children[count].className = 'serActive';
+//       }
+//       let currElem = document.getElementById(serNameArray[count]);
+//       serTabHead.innerText = currElem.innerText;
+//       serTabContent.innerHTML = services[serNameArray[count]];
+//       count++;
+//       if(count==5){
+//         setTimeout(function(){
+//           serviceAuto();
+//           count=0;
+//         },2000)
+//       }
+//   },2000);
+// }
+
+let serListArea = document.getElementById('serList');
+serListArea.addEventListener('mouseenter',serviceAutoStop)
+serListArea.addEventListener('mouseleave',serviceAuto)
+
+let serviceInterval = setInterval(function(){console.log('hello')},1000);
+
+function serviceAuto(){
+  let serviceInterval = setInterval(function(){console.log('hello')},1000);
+}
+function serviceAutoStop(){
+  clearInterval(serviceInterval);
+}
+
 
 function resetService(){
   serTabHead.innerText = 'Top Notch Services';
@@ -63,23 +111,7 @@ function showRev(){
     else{
       i = 0;
       custName.innerText = 'Krishna';
-      custReply.innerText = "The process was totally hasslefree. I'm glad that I chose gosafe for this.";
-    }
-  },3000);
-}
-
-
-function showSerTab(){
-  setInterval(function(){
-    if(i<5){
-      custName.innerText = reviews[i].name;
-      custReply.innerText = reviews[i].reply;
-      i++;
-    }
-    else{
-      i = 0;
-      custName.innerText = 'Krishna';
-      custReply.innerText = "The process was totally hasslefree. I'm glad that I chose gosafe for this.";
+      custReply.innerText = "The process was totally hasslefree. I'm glad that I chose yesgo for this.";
     }
   },3000);
 }
